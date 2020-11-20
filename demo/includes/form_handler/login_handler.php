528df678b0bec20d3465f7573bd8dbc56a84f53d
AlaaShatat <73889806+AlaaShatat@users.noreply.github.com>
2020-11-20 21:09:57 +0200

    Update login_handler.php
    php code for the login

---
+++ demo/includes/form_handler/login_handler.php
@@ -0,0 +1,34 @@
+<?php
+//get the data from the login form 
+if(isset($_POST['login_button']))
+{
+    $email=filter_var($_POST['log_email'],FILTER_SANITIZE_EMAIL);//santize email
+    $_SESSION['log_email']=$email;
+    $password= md5($_POST['log_password']);
+    $check_database= mysqli_query($con,"SELECT * FROM users WHERE email='$email' AND password='$password'");  
+    $check_login= mysqli_num_rows($check_database);
+    if($check_login==1)
+    {
+        $row=mysqli_fetch_array($check_database);
+        $username=$row['username'];
+        $_SESSION['username']=$username;
+       // $profile_pic=$row['profile_pic'];
+       header("Location: index.php");
+       exit();
+
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
+
+?>
+
