b9d956a231457a7a8d07057a80d5bd3597fe7d7a
AlaaShatat <73889806+AlaaShatat@users.noreply.github.com>
2020-12-04 15:39:09 +0200

    Update index.php
    added a header to the index and destroy session

--- demo/index.php
+++ demo/index.php
@@ -1,25 +1,7 @@
 <?php
-//connect local host to the database 
-$con =mysqli_connect("localhost","root","","social");
-// if something is wrong ,echo is used to print on the screeen ,and dot is used to connect or link  
-if(mysqli_connect_errno()){
-	echo "failed to connect:".mysqli_connect_errno(); 
-}
-echo "connected successfully";
-
+include("includes/header.php");
+session_destroy(); 
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>alaa</title>
-	<style>
-		h1{
-			color:red;
-		}
-	</style>
-</head>
-<body>
 <h1>hello there!!!!!! </h1>
 </body>
 </html>
