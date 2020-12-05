367d27d4e50129612054de50e52fa5ae10bd973f
AlaaShatat <73889806+AlaaShatat@users.noreply.github.com>
2020-12-05 19:02:13 +0200

    Update header.php
    added the top bar css

--- demo/includes/header.php
+++ demo/includes/header.php
@@ -3,6 +3,8 @@ require 'config/config.php';
 if(isset($_SESSION['username']))
 {
     $userloggedin=$_SESSION['username'];
+    $userloggedin_query=mysqli_query($con, "SELECT * FROM users WHERE username='$userloggedin' ");
+    $user= mysqli_fetch_array($userloggedin_query);
 }
 else
 {
@@ -15,10 +17,40 @@ else
 <head>
 
     <title>alaa</title>
+    <!-- js-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="assets/js/bootstrap.js"></script>
-
-	<link rel="stylesheet" type=text/css href="styling/bootstrap.css">
+<!-- css-->
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet"> 
+    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet"> 
+    <link rel="stylesheet" type=text/css href="styling/bootstrap.css">
+    <link rel="stylesheet" type=text/css href="styling/style.css">
+   
 </head>
 <body>
-   
+   <div class="top_bar">
+        <div class="logo">
+            <a href= "index.php">Alaa!</a>
+        </div>
+        <nav>
+            <a href="#">
+           <?php echo $user['first_name'];?> 
+            </a>
+            <a href="index.php">
+            <i class="icon-home icon-large"></i>
+            </a>
+            <a href="#">
+            <i class="icon-envelope"></i> 
+            </a>
+            <a href="#">
+            <i class="icon-user"></i>    
+            </a>
+            <a href="#">
+            <i class="icon-bell"></i>
+            </a>
+            <a href="#">
+            <i class="icon-cog"></i>    
+            </a>
+        </nav>
+   </div>
+   <div class="wrapper ">
