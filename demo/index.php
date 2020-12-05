f6a8454740eb2e1283450cd8d2348fe6fc9b18f1
AlaaShatat <73889806+AlaaShatat@users.noreply.github.com>
2020-12-05 19:03:04 +0200

    Update index.php
    the css for profile column and status textarea

--- demo/index.php
+++ demo/index.php
@@ -1,7 +1,37 @@
 <?php
 include("includes/header.php");
-session_destroy(); 
+//session_destroy(); 
 ?>
-<h1>hello there!!!!!! </h1>
+<div class="user_details column">
+	<a href="#"><img src="<?php echo $user['profile_pic'];?>" >
+	</a>
+	<div class="user_details_left_right">
+		<a href="#">	
+			<?php
+			echo $user['first_name']." ".$user['last_name'] ."<br>"; 
+			?>
+		</a>
+		<a>
+			<?php
+				echo"posts: " .$user['no_post']. "<br>";
+				
+				echo"likes: " .$user['no_likes']."<br>";
+			?>
+		</a>
+	</div>
+</div>
+<div class="main_column column">
+	<form action="index.php" class="post_form" method="POST">
+		<textarea name="post_text" id ="post_text" placeholder="what's in your mind?"></textarea>
+		<br>
+		<input type="submit" name="post" id="post_button" value="post">
+		<hr>
+	</form>
+
+
+
+</div>
+
+</div>
 </body>
 </html>
