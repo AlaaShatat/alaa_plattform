ca1dfff4f87ad1bd06fe50f6965728efbd871390
AlaaShatat <73889806+AlaaShatat@users.noreply.github.com>
2020-12-06 22:37:00 +0200

    Update Post.php
    fixing syntax errors

--- demo/includes/classes/Post.php
+++ demo/includes/classes/Post.php
@@ -9,24 +9,28 @@ class Post
         $this->con=$con;
         $this->user_obj=new User($con,$user);
     }
-    //get first and last name 
+    //get fpost
     public function submitpost($body, $user_to)
     {
         $body=strip_tags($body);//strip html tags
-        $body=mysqli_real_escape_string($htis->con,$body);//escape the quotes ti ignore mi$smatch
+        $body=mysqli_real_escape_string($this->con,$body);//escape the quotes ti ignore mi$smatch
+        $body=str_replace('\r\n', '\n',$body);
+        $body=nl2br($body);
         $chech_space=preg_replace('/\s+/','',$body);//erase any space
+       //check spaces
         if($chech_space !="")
         {
             //current date and time 
             $date_added=date("Y-m-d H:i:S");
             // added by 
-            $added_by=$this->$user_obj->getusername();
+            $added_by=$this->user_obj->getusername();
             if($user_to ==$added_by)
             {
                 $user_to="none";
             }
             //insert post into database
-            $insert=mysqli_queri($this->con,"INSERT INTO posts VALUES ('','$body','$added_by','$date_added','$user_to','no','no','0')");
+            $insert=mysqli_query($this->con,"INSERT INTO posts VALUES ('','$body','$added_by','$date_added','$user_to','no','no','0')");
+           // $database=mysqli_query($htis->con," INSERT INTO posts VALUES  ('','alaa','alaa','2020-6-12','alaa','no','no','0')");
             $returned_id=mysqli_insert_id($this->con);
             //insert notifiacations
             //update post count for user
