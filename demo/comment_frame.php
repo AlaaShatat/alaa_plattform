c48f6aee3ab6df97d48077074d33d8d040c5fb96
AlaaShatat <73889806+AlaaShatat@users.noreply.github.com>
2020-12-24 16:51:56 +0200

    Create comment_frame.php
    comment frame

---
+++ demo/comment_frame.php
@@ -0,0 +1,74 @@
+<html>
+<head>
+<title></title>
+<link rel="stylesheet" type=text/css href="styling/style.css">
+</head>
+<body>
+    <?php
+        require 'config/config.php';
+        //require'config/config.php';
+        include("includes/classes/User.php");
+        include("includes/classes/Post.php");
+        
+        if (isset($_SESSION['username']))
+        {
+            $userloggedin=$_SESSION['username'];
+            $user_datails=mysqli_query($con,"SELECT * FROM users WHERE username='$userloggedin'");
+            $user_fetch=mysqli_fetch_array($user_datails);
+        }
+        else 
+        {
+            header("Location:register.php");
+        }
+    ?>
+  
+    <script>
+    //toggle comment section
+        function toggle(){
+            var element = document.getElementById("comment_section");
+            if(element.style.display=="block")
+                element.style.display="none";
+            else 
+               element.style.display="block";
+        }
+    </script>
+    <?php 
+        //getid of the post  no pb
+        if(isset($_GET['post_id'])) {
+            $post_id = $_GET['post_id'];
+            
+        }
+        //fetch postid data 
+        $user_query=mysqli_query($con,"SELECT added_by,user_to FROM posts WHERE id='$post_id'");
+        $row=mysqli_fetch_array($user_query);
+        $posted_to=$row['added_by'];
+
+        //POST comment  error msh byd5ol hna asln
+        if(isset($_POST['postComment' . $post_id]))
+        {
+            echo 'hello';
+            $post_body= $_POST['post_body'];
+            $post_body=mysqli_escape_string($con, $post_body);
+            $date_time_now= date("Y:m:d H:i:s");
+            $insert_post=mysqli_query($con,"INSERT INTO comments VALUES ('', '$post_body','$userloggedin','$posted_to','$date_time_now','no','$post_id' )" );
+            echo "<p> WOohOo comment posted !</p>";
+
+
+        }
+    
+    
+    
+    
+    ?>
+    
+    <form action="comment_frame.php?post_id=<?php echo $post_id; ?>" id="comment_form" name="postComment<?php echo $post_id; ?>" method="POST"  >
+        <textarea name="post_body"></textarea>
+        <input type="submit" name="postComment <?php echo $post_id; ?>" value="post">
+    </form>
+
+</body>
+
+
+
+
+</html>
